31b680ca2d091f20bf2554feb4e9c00aac6f3adc
Sairaj Poojari <73287325+spoojari70@users.noreply.github.com>
2025-11-19 13:07:18 +0000

    FIST COMMIT AFTER UPLOADING THE pdf CODE

---
+++ test.r
@@ -0,0 +1,16 @@
+library(data.table)
+install.packages("data.table")
+
+library(data.table)
+install.packages("data.table")
+
+
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+  install.packages("ggplot2", dependencies = TRUE)
+}
+library(ggplot2)
+
+
+library(readxl)
+
+
